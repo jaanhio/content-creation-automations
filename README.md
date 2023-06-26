1f2d2f6d4244108092a098f6bac2da0bd7adb0be
Jianhao Tan <6738954+jaanhio@users.noreply.github.com>
2023-06-26 23:31:59 +0800

    added cinematic look conversion

--- README.md
+++ README.md
@@ -2,13 +2,21 @@
 
 Just documenting down how to use `imagemagick` to automate the monotonous process of adding borders and resizing images in order to fit a photo nicely onto instagram profile page.
 
+Also added command to go for a cinematic look.
+
 To use:
 ```
-./convert.sh $image_dir
+./convert.sh $image_dir $option
 ```
+`option = 1` will generate a film look white borders
+`option = 2` will generate a cinematic look
 
-This converts an image from this:
+Before:
 ![before](./before.jpg)
 
-to this:
+After option 1:
 ![after](./after.jpg)
+
+After option 2:
+![after-cinematic](./after-cinematic.jpg)
+
--- convert.sh
+++ convert.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 image_dir=$1
+conversion_type=$2 # option1. film or option2. cinematic
 
 if [ -z $image_dir ]; then
     echo "Please provide image directory as first argument."
@@ -9,6 +10,16 @@ fi
 
 if [ ! -d $image_dir ]; then
     echo "Not a valid directory"
+    exit 1
+fi
+
+if [ -z $conversion_type ]; then
+    echo "conversion_type not set. setting it to default film conversion"
+    conversion_type=1
+fi
+
+if [ ! $conversion_type -eq 1 ] && [ ! $conversion_type -eq 2 ]; then
+    echo "Conversion type must be either 1 or 2"
     exit 1
 fi
 
@@ -21,5 +32,10 @@ mkdir $output_dir
 for image in $image_dir/*; do
     echo "Processing $image"
     file_name=$(basename $image)
-    magick convert -bordercolor white -border 100 -gravity center -resize 1200x1200 -extent 1200x1200 $image $output_dir/$file_name
+
+    if [ $conversion_type -eq 1 ]; then
+        magick convert -bordercolor white -border 100 -gravity center -resize 1200x1200 -extent 1200x1200 $image $output_dir/$file_name
+    else
+        magick convert -gravity center -crop 100%x78% -bordercolor black -border 0%x14.1% $image $output_dir/$file_name
+    fi
 done
